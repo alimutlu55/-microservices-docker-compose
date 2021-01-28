95a1360a2e05bb78498629449ed114ea007a69e4
almtl55 <alimutlu_5529@icloud.com>
2021-01-29 01:09:31 +0300

    serviceRegistritionServer added

---
+++ serviceRegistrationServer/src/test/java/com/microservice/serviceRegistrationServer/ServiceRegistrationServerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.microservice.serviceRegistrationServer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationServerApplicationTests {
+
+
+}
