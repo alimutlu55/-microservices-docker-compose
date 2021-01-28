d38fa53426ded5434519b49a4da6a5a49d840c51
almtl55 <alimutlu_5529@icloud.com>
2021-01-29 00:55:00 +0300

    movieServer added

---
+++ movieServer/src/main/java/com/server/movieServer/MovieController.java
@@ -0,0 +1,25 @@
+package com.server.movieServer;
+
+
+import com.server.movieServer.models.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class MovieController {
+
+    @GetMapping("/list")
+    public List<Movie> getAllMovies() {
+
+        List<Movie> movies = new ArrayList<>();
+
+        movies.add(new Movie("Kelebek Etkisi", "2004", "7,7"));
+        movies.add(new Movie("Zamanın Ötesinde", "2014", "7,5"));
+
+        return movies;
+    }
+}
---
+++ movieServer/src/test/java/com/server/movieServer/MovieServerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.server.movieServer;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServerApplicationTests {
+
+}
